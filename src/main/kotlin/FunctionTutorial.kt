09beba87f4b1d9e2f96008b97dce2b9560e9f8a3
jhlee <leejinhyung90@gmail.com>
2022-05-14 22:45:31 +0900

    타입추론과 함수

---
+++ src/main/kotlin/FunctionTutorial.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    println("함수선언")
+
+    println(add(5, 6, 7))
+    println(addSingleExpressionFunction(5, 6, 7))
+
+}
+
+// fun 함수이름(매개변수1: 변수타입1, 매개변수2: 변수타입2 ...): 반환형타입 (없으면 생략가능)
+fun add(a: Int, b: Int, c: Int): Int {
+    return a + b + c
+}
+
+// 단일 표현식 함수 : 반환형을 타입추론 가능하므로 생락가능
+fun addSingleExpressionFunction(a: Int, b: Int, c: Int) = a + b + c
---
+++ src/main/kotlin/TypeInferenceTutorial.kt
@@ -0,0 +1,32 @@
+fun main() {
+
+    println("타입추론과 함수")
+
+    var a = 1234
+    // Int 형 변수로 지정
+
+    var b = 1234L
+    // Long 형 변수로 지정
+
+    var c = 12.45
+    // Double 형 변수로 지정
+
+    var d = 12.45f
+    // Float 형 변수로 지정
+
+    var e = 0xABCD
+    var f = 0b0101010
+    // 16진수, 2진수 -> Int형으로 추론
+
+    var g = true
+    // Boolean 형 변수로 지정
+
+    var h = 'c'
+    // Char 형 변수로 지정
+
+    // 반드시 특정한 자료형으로 지정해야하는 상황이 아니라면
+    // 대부분은 코틀린의 타입추론 기능을 이용하여 코드량을 줄일수 있음
+
+    println("result : $a, $b, $c, $d, $e, $f, $g, $h")
+    
+}
